4cdf7c45e48b87f7189ffe39f4cccac1e54d1705
Zahan Malkani <zahanm@gmail.com>
2012-07-26 15:35:32 -0400

    new dogpy syntax

--- ranking.py
+++ ranking.py
@@ -3,7 +3,8 @@ import sys
 import json
 import dogpy
 
-def rank_k_teachers(me, teachers, k):
+@dogpy.extfunc
+def rank_k_teachers(me, teachers, k=5):
   def combined_score(t):
     overlap_teach = len(set(t.teachables) | set(me.learnables))
     overlap_learn = len(set(t.learnables) | set(me teachables))
@@ -12,21 +13,4 @@ def rank_k_teachers(me, teachers, k):
   return teachers[:k]
 
 if __name__ == '__main__':
-  if dogpy.valid_call:
-    me, teachers, k = dogpy.argv
-    top_k = rank_k_teachers(me, teachers, k)
-    dogpy.output(top_k)
-  else:
-    dogpy.error("Bad arguments")
-
-# ===============
-#
-# if len(sys.argv) != 4:
-#   print(json.dumps({ success:false }))
-# else:
-#   inp = json.loads(sys.stdin.read())
-#   me = inp[0]
-#   teachers = inp[1]
-#   k = inp[2]
-#   top_k = rank_k_teachers(me, teachers, k)
-#   json.dump(top_k, sys.stdout)
+  dogpy.run()
