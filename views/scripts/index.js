eec7cbc9bba35610073d5164735cfa26234a1be0
Zahan Malkani <zahanm@gmail.com>
2012-07-28 15:39:00 -0400

    thanks that fades out

--- views/scripts/index.js
+++ views/scripts/index.js
@@ -25,6 +25,24 @@ dogjs.on('load', function () {
   // no need to defer: we wait for `dogjs` to load
   cycle();
 
+  function fadeOut(elem) {
+    elem.style.opacity = '1';
+    timegap = 100;
+    delta = -0.1;
+    function fadeDelta(elem, amount) {
+      if (Math.abs(Number(elem.style.opacity) + amount) < 0.00001) {
+        elem.style.opacity = '0';
+      }
+      else {
+        elem.style.opacity = Number(elem.style.opacity) + amount;
+      }
+      if (Number(elem.style.opacity) > 0) {
+        setTimeout(fadeDelta.bind(this, elem, delta), timegap);
+      }
+    }
+    setTimeout(fadeDelta.bind(this, elem, delta), timegap);
+  }
+
   var button = document.querySelector('#start-finding > button');
   button.addEventListener('click', function () {
     document.querySelector('#state-teachable').style.display = 'block';
@@ -37,6 +55,8 @@ dogjs.on('load', function () {
   button.addEventListener('click', function () {
     document.querySelector('#state-teachable').style.display = 'none';
     document.querySelector('#identify-themself').style.display = 'none';
+    document.querySelector('#thanks-holder').style.display = 'block';
+    fadeOut(document.querySelector('#thanks-holder'));
   });
   button = document.querySelector('#admin-button');
   button.addEventListener('click', function () {
