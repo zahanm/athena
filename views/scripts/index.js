438dd01fcaaab22b21ab644a9d35ffb4a5c60afb
Zahan Malkani <zahanm@gmail.com>
2012-08-06 14:29:22 -0400

    customevents for submit, emits post signup

--- views/scripts/index.js
+++ views/scripts/index.js
@@ -25,40 +25,51 @@ dogjs.on('load', function () {
   // no need to defer: we wait for `dogjs` to load
   cycle();
 
+  function changePlaceholder(elem, ptext) {
+  }
+
   function fadeOut(elem) {
     elem.style.opacity = '1';
     timegap = 100;
     delta = -0.1;
     function fadeDelta(elem, amount) {
-      if (Math.abs(Number(elem.style.opacity) + amount) < 0.00001) {
-        elem.style.opacity = '0';
+      var opacity = Number(elem.style.opacity);
+      opacity += amount;
+      if (opacity <= 0.1) {
+        opacity = 0;
       }
-      else {
-        elem.style.opacity = Number(elem.style.opacity) + amount;
-      }
-      if (Number(elem.style.opacity) > 0) {
+      elem.style.opacity = String(opacity);
+      if (opacity > 0) {
         setTimeout(fadeDelta.bind(this, elem, delta), timegap);
+      } else {
+        elem.style.display = 'none';
       }
     }
     setTimeout(fadeDelta.bind(this, elem, delta), timegap);
   }
 
-  var button = document.querySelector('#start-finding > button');
-  // button.addEventListener('click', function () {
-  //   document.querySelector('#state-teachable').style.display = 'block';
-  // });
-  // button = document.querySelector('#state-teachable > button');
-  // button.addEventListener('click', function () {
-  //   document.querySelector('#identify-themself').style.display = 'block';
-  // });
-  button = document.querySelector('#identify-themself > button[type="submit"]');
-  button && button.addEventListener('click', function () {
-    // document.querySelector('#state-teachable').style.display = 'none';
-    // document.querySelector('#identify-themself').style.display = 'none';
-    document.querySelector('#thanks-holder').style.display = 'block';
-    fadeOut(document.querySelector('#thanks-holder'));
-  });
-  button = document.querySelector('#admin-button');
+  function setupHandlers(elem) {
+    if (elem.attributes['ask'] && elem.attributes['ask'].value === "identify_yourself") {
+      // this is the identify_yourself form
+      elem.addEventListener('submitted:ask', function () {
+        var step = document.querySelector('form[listen="goals"]');
+        step && step.reset();
+        ['form[ask="state_skill_needed"]', 'form[ask="state_teachable"]', 'form[ask="identify_yourself"]'].forEach(function (selector) {
+          step = document.querySelector(selector);
+          if (step) {
+            step.reset();
+            step.style.display = 'none';
+          }
+        });
+        document.querySelector('#thanks-holder').style.display = 'block';
+        fadeOut(document.querySelector('#thanks-holder'));
+      });
+    }
+  }
+
+  dogjs.on('add:node', setupHandlers);
+
+  var button = document.querySelector('#admin-button');
   button.addEventListener('click', function () {
     var holder = document.querySelector('#admin-holder');
     if (holder.style.display && holder.style.display === 'block') {
