098c72c90c43c4bd2e56f505f06fad22005b7dfa
Zahan Malkani <zahanm@gmail.com>
2012-08-27 14:07:56 -0400

    transformed index.html to simple page model, placeholder cycling

--- views/scripts/index.js
+++ views/scripts/index.js
@@ -4,43 +4,86 @@ dogjs.on('pageload', function () {
   // placeholder cycling
   // -------------------
 
-  var examples, i, interval;
-  examples = [
-    'learn to rap',
-    'cook a thanksgiving dinner',
-    'hike the Dish',
-    'build your own website'
+  var exampleProjects, exampleSkills, interval, carousels;
+  exampleProjects = [
+    'make a thanksgiving dinner',
+    'climb the Dish',
+    'drop some rhymes',
+    'build my own website'
   ];
-  i = 0;
+  exampleSkills = [
+    'cook',
+    'hike',
+    'rap',
+    'programming'
+  ];
   interval = 2000; // in ms
-  function cycle(argument) {
-    // cycle through placeholders for learnables
-    var learnable, teachable;
-    learnable = document.querySelector('#conversation input[name="skill"]');
-    teachable = document.querySelector('#conversation input[name="teachable"]');
-    [ learnable, teachable ].forEach(function (elem) {
+  carousels = {};
+
+  function setupCarousel(selector, parade) {
+    var elem = document.querySelector(selector);
+    if (!elem || carousels[selector]) { return }
+    carousels[selector] = true;
+    var i = 0;
+    function cycle() {
       if (elem && document.activeElement !== elem && elem.value === '') {
-        elem.placeholder = examples[i];
-        i = (i+1) % examples.length;
+        elem.placeholder = parade[i];
+        i = (i+1) % parade.length;
       }
-    });
-    // setTimeout(cycle, interval);
+    }
+    setInterval(cycle, interval);
   }
-  // no need to defer: we wait for `dogjs` to load
-  cycle();
+
+  // cycle through placeholders for learnables
+  function checkAllCarousels() {
+    [ '#conversation input[name="value"]', '#conversation input[name="skill"]', '#conversation input[name="teachable"]' ].forEach(function (s, i) {
+      setupCarousel(s, (i == 0) ? exampleProjects: exampleSkills);
+    });
+  }
+  dogjs.on('add:node', checkAllCarousels);
+  checkAllCarousels();
 
   function changePlaceholder(elem, ptext) {
   }
 
-  // setting up event handlers
-  // -------------------------
+  // Thank you banner
+  // ----------------
 
-  var changer = document.querySelector('#change-page');
-  changer && changer.addEventListener('submit', function (ev) {
-    ev.preventDefault();
-    destination = changer.querySelector('input[type="text"]');
-    dogjs.changePage(destination.value);
-    return false;
+  function fadeOut(elem, callback) {
+    elem.style.opacity = '1';
+    timegap = 100;
+    delta = -0.1;
+    function fadeDelta(elem, amount) {
+      var opacity = Number(elem.style.opacity);
+      opacity += amount;
+      if (opacity <= 0.1) {
+        opacity = 0;
+      }
+      elem.style.opacity = String(opacity);
+      if (opacity > 0) {
+        setTimeout(fadeDelta.bind(this, elem, delta), timegap);
+      } else {
+        elem.style.display = 'none';
+        callback && callback();
+      }
+    }
+    setTimeout(fadeDelta.bind(this, elem, delta), timegap);
+  }
+
+  // this is the identify_yourself form submission
+  dogjs.on('submitted:ask:identify_yourself', function (data) {
+    ['form[listen="goals"]', 'form[ask="state_skill_needed"]', 'form[ask="state_teachable"]', 'form[ask="identify_yourself"]'].forEach(function (selector) {
+      step = document.querySelector(selector);
+      if (!step) { return; }
+      step.reset();
+      step.style.display = 'none';
+    });
+    document.querySelector('#thanks-holder').style.display = 'block';
+    setTimeout(function () {
+      fadeOut(document.querySelector('#thanks-holder'), function () {
+        dogjs.changePage('browse');
+      });
+    }, 2000);
   });
 
-});
+}, this, true);
